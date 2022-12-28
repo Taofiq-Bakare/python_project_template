d2de00bf0486d82d5d980c6efba5896a3077dd03
Taofiq-Bakare <taofiq01@gmail.com>
2022-12-29 07:39:46 +0800

    Fix: added docstring to function

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -3,12 +3,21 @@ import numpy as np
 
 
 def add_x(x: float, y: float) -> float:
+    """
+    A trivial implementation of the add function.
+
+    Args:
+        x (float): the first value to add
+        y (float): the second value to add
+
+    Raises:
+        ValueError: Raises error if the value is not float
+
+    Returns:
+        float: floating point value.
+    """
     try:
         res = np.float32(x) + np.float32(y)
     except ValueError:
         raise ValueError("Please provide float/int")
     return res
-
-
-def get_name(name: str):
-    print(name)
