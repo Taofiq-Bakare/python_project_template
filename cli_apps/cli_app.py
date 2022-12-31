83e0f3a5a73c7dc1915ecc61d9183ccb2ebd11eb
Taofiq-Bakare <taofiq01@gmail.com>
2022-12-31 20:22:28 +0800

    updated some few packages

--- cli_apps/cli_app.py
+++ cli_apps/cli_app.py
@@ -1,11 +1,24 @@
+"""
+The module for the command line tools
+"""
+
 import click
-import utils.helper_functions as helper_functions
+
+from utils import helper_functions
+
+# import utils.helper_functions as helper_functions
 
 
 @click.command()
 @click.option("--first_val", prompt="first_val", help="The first value to add")
 @click.option("--second_val", prompt="second_val", help="The second value to add")
 def add_x(first_val: float, second_val: float):
+    """_summary_
+
+    Args:
+        first_val (float): _description_
+        second_val (float): _description_
+    """
     result = helper_functions.add_x(first_val, second_val)
     click.echo(result)
 
